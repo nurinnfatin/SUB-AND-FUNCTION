8f6cd6fd5e4a4dd6c69a9fee3900bcea32fb27e0
Nurinn Fatin Binti Norazlan <18DDT19F1038@student.ptss.edu.my>
2021-04-20 22:46:22 +0800

    FUNCTION AND SUB

--- SUB AND FUNCTION/Form1.vb
+++ SUB AND FUNCTION/Form1.vb
@@ -1,24 +1,39 @@
 ﻿Public Class Form1
-    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+    Private Sub S1()
         lbl1.Text = "Submit"
     End Sub
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        S1()
+    End Sub
 
-    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+    Private Sub S2()
         Dim strText As String
         strText = txt1.Text
         lbl2.Text = strText
     End Sub
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        S2()
+    End Sub
+
+    Private Function F1()
+        Return "DATA TERSEBUT"
+    End Function
 
     Private Function Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
-        lbl3.Text = "Submit"
-        Return lbl3
+        lbl3.Text = F1()
+
+    End Function
+
+    Private Function F2()
+        Dim strText2 As String
+        strText2 = txt2.Text
+        Return strText2
+
     End Function
 
     Private Function Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
-        Dim strText2 As String
-        strText2 = txt2.Text
-        lbl4.Text = txt2.Text
-        Return lbl4
 
+        lbl4.Text = F2()
     End Function
+
 End Class
